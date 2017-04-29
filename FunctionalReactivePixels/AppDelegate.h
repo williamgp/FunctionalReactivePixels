9331bfc70f3e795598aa35048a1734a2231159ec
William Peregoy <williampergoy@gmail.com>
2017-04-29 18:39:59 +0800

    Initial Commit

---
+++ FunctionalReactivePixels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunctionalReactivePixels
+//
+//  Created by William Peregoy on 29/04/2017.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunctionalReactivePixels/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunctionalReactivePixels
+//
+//  Created by William Peregoy on 29/04/2017.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
