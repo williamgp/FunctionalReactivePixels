ac234e764e7db38d6fdfed2868ef1d6edaa2bea2
William Peregoy <william@Williams-MacBook-Air.local>
2017-06-05 11:54:19 +0700

    starting on FRPGalleryViewModel

---
+++ FunctionalReactivePixels/FRPGalleryViewModel.h
@@ -0,0 +1,13 @@
+//
+//  FRPGalleryViewModel.h
+//  FunctionalReactivePixels
+//
+//  Created by William Peregoy on 6/5/17.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+#import <ReactiveViewModel/ReactiveViewModel.h>
+
+@interface FRPGalleryViewModel : RVMViewModel
+
+@end
---
+++ FunctionalReactivePixels/FRPGalleryViewModel.m
@@ -0,0 +1,13 @@
+//
+//  FRPGalleryViewModel.m
+//  FunctionalReactivePixels
+//
+//  Created by William Peregoy on 6/5/17.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+#import "FRPGalleryViewModel.h"
+
+@implementation FRPGalleryViewModel
+
+@end
